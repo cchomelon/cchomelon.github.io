722cafae7be13cb72ee2fd325d1d91637cbd6d10
komatsu <komatsu@komatsu.local>
2026-01-19 18:26:04 +0800

    First version

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,16 @@
 module.exports = function (eleventyConfig) {
+  const markdownIt = require("markdown-it");
+  const { katex } = require("@mdit/plugin-katex");
+  const { DateTime } = require("luxon");
+
   eleventyConfig.addPassthroughCopy({ "public": "/" });
+  eleventyConfig.addFilter("utcDate", (value) => {
+    return DateTime.fromJSDate(value, { zone: "utc" }).toFormat("dd LLL yy 'UTC'");
+  });
+  eleventyConfig.setLibrary(
+    "md",
+    markdownIt({ html: true, linkify: true }).use(katex, { output: "mathml" })
+  );
 
   return {
     dir: {
